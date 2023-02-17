682f8d01c8a6657c20a0b4d2dd761c142e2a880d
bkbalta2023 <124371242+bkbalta2023@users.noreply.github.com>
2023-02-17 20:29:08 +0300

    Add files via upload

---
+++ UNI-NOT.py
@@ -0,0 +1,24 @@
+note = float(input("Notunuzu Giriniz:"))
+
+if note >100:
+    print("Lutfen gecerli bir not giriniz")
+elif (note >=90):
+    print("AA")
+elif note >=85:
+    print("BA")
+elif note >=80:
+    print("BB")
+elif note >= 75:
+    print("CB")
+elif note >=70:
+    print("CC")
+elif note >= 65:
+    print("DC")
+elif note >= 60:
+    print("DD")
+elif note >= 50:
+    print("FD")
+elif note >= 0:
+    print("FF")
+else:
+    print("Lutfen gecerli bir not giriniz")
